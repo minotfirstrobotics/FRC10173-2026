a1b4ef9371b03ead85dd4a124edda7053caf9b9b
Aaron K <dedios8@yahoo.com>
2026-03-13 18:19:21 -0500

    Added SEQ_Shoot

---
+++ commands/CMD_Shoot.py
@@ -0,0 +1,14 @@
+from commands2 import SequentialCommandGroup, WaitCommand
+from subsystems.SS_ShooterNEO import SpinUpAndWait_CommDef
+from subsystems.SS_FeederTalon_Power import SS_FeederTalon_Power
+from subsystems.SS_ShooterNEO import SS_ShooterNEO
+
+class CMD_Shoot(SequentialCommandGroup):
+    """
+
+    """
+    def __init__(self, shooter: SS_ShooterNEO, feeder: SS_FeederTalon_Power):
+        super().__init__(
+            SpinUpAndWait_CommDef(shooter),
+            feeder.run_forward_command().withTimeout(3.0) # Run feeder for 3 seconds after shooter is up to speed
+        )
