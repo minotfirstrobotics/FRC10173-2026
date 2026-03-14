642cdb12cde4889084af9a192a783a6a412ad5a2
Aaron K <dedios8@yahoo.com>
2026-03-14 16:17:26 -0500

    Recommended reset_pose change based on available functions. Changed SS_SwerveDrive AutoBuilder controller PID values based on other teams usage.

--- subsystems/SS_SwerveDrive.py
+++ subsystems/SS_SwerveDrive.py
@@ -63,8 +63,8 @@ class SS_SwerveDrive(commands2.Subsystem):
             robot_relative_speeds_supplier=self.get_robot_relative_speeds,
             output=self.drive_robot_relative,
             controller=PPHolonomicDriveController(
-                PIDConstants(5.0, 0.0, 0.0),  # Translation PID (tune these values)
-                PIDConstants(5.0, 0.0, 0.0),  # Rotation PID (tune these values)
+                PIDConstants(0.0, 0.0, 0.0),  # Translation PID (tune these values)
+                PIDConstants(0.0, 0.0, 0.0),  # Rotation PID (tune these values)
             ),
             robot_config=RobotConfig.fromGUISettings(),
             should_flip_path=lambda: DriverStation.getAlliance() == DriverStation.Alliance.kRed,
@@ -112,7 +112,7 @@ class SS_SwerveDrive(commands2.Subsystem):
 
     def reset_pose(self, pose: Pose2d) -> None:
         """Reset the robot's odometry to the specified pose."""
-        self.drivetrain.reset_odometry(pose)
+        self.drivetrain.reset_odometry(pose) # maybe this should be reset_pose(pose) rather than reset_odometry from swerve_drivetrain.py
         self._latest_pose = pose
 
     def get_robot_relative_speeds(self) -> ChassisSpeeds:
