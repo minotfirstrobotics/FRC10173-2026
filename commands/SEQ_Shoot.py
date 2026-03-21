3794b92655a6009e05bf4b7ed34808d7790039ac
FIRST2 <dedios8@yahoo.com>
2026-03-21 14:34:07 -0500

    Switched Shooter Kraken out for Shooter NEO

--- commands/SEQ_Shoot.py
+++ commands/SEQ_Shoot.py
@@ -1,12 +1,12 @@
 from commands2 import SequentialCommandGroup, WaitCommand
 from subsystems.SS_FeederTalon_Power import SS_FeederTalon_Power
-from subsystems.SS_ShooterKraken import SS_ShooterNEO, SpinUpAndWait_CommDef
+from subsystems.SS_ShooterKraken import SS_ShooterKraken, SpinUpAndWait_CommDef
 
 class SEQ_Shoot(SequentialCommandGroup):
     """
 
     """
-    def __init__(self, shooter: SS_ShooterNEO, feeder: SS_FeederTalon_Power):
+    def __init__(self, shooter: SS_ShooterKraken, feeder: SS_FeederTalon_Power):
         super().__init__(
             SpinUpAndWait_CommDef(shooter),
             feeder.run_forward_command().withTimeout(3.0) # Run feeder for 3 seconds after shooter is up to speed
