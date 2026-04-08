0b82b9ba05bd6d388f88ff0807dd4bf7da3dd3f4
Nickolas Franco <nickolasfranco618@gmail.com>
2026-04-08 16:22:26 -0500

    rename to test auto commands

--- robot.py
+++ robot.py
@@ -71,7 +71,7 @@ class RobotContainer:
     def _build_complex_commands_and_autochooser(self):
         self.cmd_combo_shoot = CMD_ComboShoot(self.ss_shooter, self.ss_feeder, self.ss_swerve_drive)
         SmartDashboard.putData("Commands/Combo Shoot", self.cmd_combo_shoot)
-        NamedCommands.registerCommand("Commands/Combo Shoot", self.cmd_combo_shoot)
+        NamedCommands.registerCommand("Combo Shoot", self.cmd_combo_shoot)
 
         # self.seq_shoot = SEQ_shoot(self.ss_shooter, self.ss_feeder)
         # NamedCommands.registerCommand("Commands/SEQ Shoot", self.seq_shoot)
